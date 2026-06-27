c81c99e78c745714abbd5253329a8a467c3b974f
Kiarash_haghi <kiarash90haghi@gmail.com>
2026-06-27 10:22:24 +0330

    v2.2

--- JS/mobile.js
+++ JS/mobile.js
@@ -11,3 +11,15 @@ hearts.forEach(heart => {
     });
 
 });
+
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const sidebar = document.querySelector(".side-bar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
